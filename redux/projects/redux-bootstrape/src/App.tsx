85d7dbc8e95ebcfebe175bc7046ad11a1cc4ec94
muhamsh <muhammad-ashraful@outlook.com>
2025-06-28 16:26:49 +0600

    merging

--- redux/projects/redux-bootstrape/src/App.tsx
+++ redux/projects/redux-bootstrape/src/App.tsx
@@ -1,9 +1,11 @@
+import Nav from "./components/layouts/Nav"
 import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Nav/>
       <Button className="cursor-pointer" variant={"outline"}>Test button</Button>
     </div>
   )
---
+++ redux/projects/redux-bootstrape/src/components/layouts/Nav.tsx
@@ -0,0 +1,10 @@
+
+export default function Nav() {
+    return (
+        <nav className='max-w-7xl mx-auto h-16 flex items-center gap-3 px-5'>
+            <div className='flex items-center'>
+               <span className='font-bold ml-2'>ToDo</span>
+            </div>
+        </nav>
+    );
+}
