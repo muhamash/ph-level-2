98378048a60bb75feeac038dd3ae77cbab8faeba
muhamsh <muhammad-ashraful@outlook.com>
2025-06-29 18:20:10 +0600

    ........

--- redux/projects/redux-bootstrape/src/App.tsx
+++ redux/projects/redux-bootstrape/src/App.tsx
@@ -1,14 +1,16 @@
-import Nav from "./components/layouts/Nav"
-import { Button } from "./components/ui/button"
+import Nav from "./components/layouts/Nav";
+import { Button } from "./components/ui/button";
 
 function App() {
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Nav/>
-      <Button className="cursor-pointer" variant={"outline"}>Test button</Button>
+    <div>
+      <Nav />
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button className="cursor-pointer" variant={"outline"}>Test button</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- redux/projects/redux-bootstrape/src/components/layouts/Nav.tsx
+++ redux/projects/redux-bootstrape/src/components/layouts/Nav.tsx
@@ -1,4 +1,3 @@
-
 export default function Nav() {
     return (
         <nav className='max-w-7xl mx-auto h-16 flex items-center gap-3 px-5'>
